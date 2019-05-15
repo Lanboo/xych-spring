8b23b37fb75c97d9736b8230f3489e45d0b146a2
Lanboo <549434510@qq.com>
2019-05-15 16:05:09 +0800

    INIT simple-spring-mvc v2

---
+++ simple-spring-mvc/src/main/java/com/xych/simple/spring/mvc/framework/v2/servlet/DispatcherServlet.java
@@ -0,0 +1,18 @@
+package com.xych.simple.spring.mvc.framework.v2.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DispatcherServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
